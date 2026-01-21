23303a2142b9c0c444929515fe82f355f1e26ea7
rshindle <rshindle@truecar.com>
2026-01-21 13:16:09 -0800

    initial commit

---
+++ src/test/java/dev/irontech/idempotent/IdempotentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.irontech.idempotent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
